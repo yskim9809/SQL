aad4b5cb9f6927774c65e6ad78d04e491d09ad64
yskim9809 <dudtj9809@gmail.com>
2025-09-19 22:06:32 +0900

    20250919

--- GroupBy.sql
+++ GroupBy.sql
@@ -11,3 +11,13 @@ where b.AUTHOR_ID=a.AUTHOR_ID and b.BOOK_ID=s.BOOK_ID
 and to_char(s.sales_date,'yyyy-mm') = '2022-01'
 group by a.author_id, a.author_name, b.category
 order by a.author_id, b.category desc
+
+--식품분류별 가장 비싼 식품의 정보 조회하기
+select t.category CATEGORY, t.MAX_PRICE MAX_PRICE, f.product_name PRODUCT_NAME
+from FOOD_PRODUCT f,
+(SELECT category, max(price) as MAX_PRICE
+from FOOD_PRODUCT
+where category in ('과자', '국', '김치','식용유')
+group by category) t
+where f.price = t.MAX_PRICE and f.category = t.category
+order by MAX_PRICE desc
