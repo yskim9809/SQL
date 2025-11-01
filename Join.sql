0c5959d015ac6efd2ebadc59860bf1c5b9e0d352
yskim9809 <dudtj9809@gmail.com>
2025-11-01 21:30:18 +0900

    20251101

--- Join.sql
+++ Join.sql
@@ -8,3 +8,13 @@ SELECT p.PRODUCT_CODE as PRODUCT_CODE, sum(p.PRICE*o.SALES_AMOUNT) as SALES
 from PRODUCT p join OFFLINE_SALE o on p.PRODUCT_ID = o.PRODUCT_ID
 group by p.PRODUCT_CODE
 order by SALES desc, PRODUCT_CODE asc
+
+/*Lv.3*/
+
+--오랜 기간 보호한 동물(1)
+SELECT i.name, i.datetime
+from animal_ins i
+left join animal_outs o
+on i.animal_id=o.animal_id
+where o.datetime is null
+order by i.datetime FETCH FIRST 3 ROWS ONLY;
