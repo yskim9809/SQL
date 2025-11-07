5532bdba15650a2e1fb4adbf53703483dcf96473
yskim9809 <dudtj9809@gmail.com>
2025-11-07 23:39:27 +0900

    20251107

--- Join.sql
+++ Join.sql
@@ -34,6 +34,12 @@ on i.animal_id=o.animal_id
 where i.animal_id is null
 order by o.animal_id;
 
+--있었는데요 없었습니다
+SELECT i.animal_id, i.name
+from animal_ins i, animal_outs o
+where i.animal_id=o.animal_id and i.datetime>o.datetime
+order by i.datetime;
+
 
 /*Lv.4*/
 
