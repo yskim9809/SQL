f30e432eda5d3eef6625e794f28622cc95c5bacf
yskim9809 <dudtj9809@gmail.com>
2025-11-21 00:01:49 +0900

    20251121

--- Select.sql
+++ Select.sql
@@ -79,6 +79,13 @@ order by factory_id asc;
 
 /*Lv.2*/
 
+--재구매가 일어난 상품과 회원 리스트 구하기
+SELECT user_id, product_id
+from online_sale
+group by user_id, product_id
+having count(*)>1
+order by user_id, product_id desc;
+
 --업그레이드 된 아이템 구하기
 select t.ITEM_ID, ii.ITEM_NAME, ii.RARITY
 from ITEM_TREE t,
