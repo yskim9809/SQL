d84ac08162ce13a5e80289b14d041bbbc9296063
yskim9809 <dudtj9809@gmail.com>
2025-12-12 23:14:14 +0900

    20251212

--- SumMaxMin.sql
+++ SumMaxMin.sql
@@ -11,3 +11,23 @@ from PRODUCT
 --최댓값 구하기
 SELECT max(datetime)
 from ANIMAL_INS
+
+--잡은 물고기 중 가장 큰 물고기의 길이 구하기
+# select concat(length,'cm') as MAX_LENGTH
+# from FISH_INFO 
+# where length = (select max(length) from fish_info)
+
+select concat(max(length), 'cm') as MAX_LENGTH
+from fish_info
+
+/*Lv.2*/
+
+
+--가격이 제일 비싼 식품의 정보 출력하기
+-- SELECT *
+-- from (select * from FOOD_PRODUCT order by price desc)
+-- where rownum =1
+
+select * 
+from FOOD_PRODUCT 
+where price = (select max(price) from FOOD_PRODUCT )
